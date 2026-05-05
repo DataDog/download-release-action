5a60c58b4b70b5e6c5ba74ac7c5359873a349999
Bruce Bujon <bruce.bujon@datadoghq.com>
2026-05-05 11:03:31 +0200

    feat: Improve error handling

--- src/main.ts
+++ src/main.ts
@@ -12,39 +12,35 @@ interface OctokitOptions {
 }
 
 async function run(): Promise<void> {
-  try {
-    // Create octokit client
-    const token = core.getInput('github-token', {required: true})
+  // Create octokit client
+  const token = core.getInput('github-token', {required: true})
   const debug = core.getBooleanInput('debug')
-    const opts = {} as OctokitOptions
+  const opts = {} as OctokitOptions
   if (debug) {
-      opts.log = console
-    }
-    const github = getOctokit(token, opts)
-    // Try to update all releases
-    const releases = await listReleases(github)
-    if (releases.length === 0) {
-      core.info('ℹ️ No release found')
-    } else {
-      core.info(`ℹ️ ${releases.length} release found.`)
-    }
-    const updatedReleases = []
-    for (const release of releases) {
-      if (release.needUpdate()) {
-        core.info(
-          `ℹ️ Release ${release.tagName()} needs an update (${release.currentVersion?.toString()} => ${release.latestVersion?.toString()}).`
-        )
-        await updateRelease(github, release)
-        core.info(`✅ Release ${release.tagName()} was successfully updated.`)
-        updatedReleases.push(release)
-      } else {
-        core.info(`✅ Release ${release.tagName()} is up-to-date.`)
-      }
-    }
-    core.setOutput('updated-releases', updatedReleases)
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(`❌ ${error.message}`)
+    opts.log = console
   }
+  const github = getOctokit(token, opts)
+  // Try to update all releases
+  const releases = await listReleases(github)
+  if (releases.length === 0) {
+    core.info('ℹ️ No release found')
+  } else {
+    core.info(`ℹ️ ${releases.length} release found.`)
+  }
+  const updatedReleases = []
+  for (const release of releases) {
+    if (release.needUpdate()) {
+      core.info(
+        `ℹ️ Release ${release.tagName()} needs an update (${release.currentVersion?.toString()} => ${release.latestVersion?.toString()}).`
+      )
+      await updateRelease(github, release)
+      core.info(`✅ Release ${release.tagName()} was successfully updated.`)
+      updatedReleases.push(release)
+    } else {
+      core.info(`✅ Release ${release.tagName()} is up-to-date.`)
+    }
+  }
+  core.setOutput('updated-releases', updatedReleases)
 }
 
-run()
+run().catch(error => core.setFailed(`❌ ${error instanceof Error ? error.message : String(error)}`))
